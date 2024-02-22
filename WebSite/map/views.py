5a6d6dc9c8a6a1e711f6b9a5d950ed1b591bf5f7
Max Zhylka <dexhonesta@gmail.com>
2024-02-22 18:05:42 +0200

    Created Search

--- WebSite/map/views.py
+++ WebSite/map/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Cities
 from .models import Roads
-# Create your views here.
+
+
+
 
 def Name(request):
     cities=Cities.objects.all()
     roads=Roads.objects.all()
+
     return render(request, "map/map.html",{'cities': cities, 'roads': roads})
 
