5991f322e97cb0fc9fe1b2f3a97d3e8a8bd4020e
Max Zhylka <dexhonesta@gmail.com>
2024-02-23 02:33:18 +0200

    Change road's color, added db from registration, fixed city search

--- WebSite/map/views.py
+++ WebSite/map/views.py
@@ -2,13 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Cities
 from .models import Roads
-
-
+from registration.models import Character
 
 
 def Name(request):
     cities=Cities.objects.all()
     roads=Roads.objects.all()
-
-    return render(request, "map/map.html",{'cities': cities, 'roads': roads})
+    characters=Character.objects.all()
+    return render(request, "map/map.html",{'cities': cities, 'roads': roads, 'characters': characters})
 
