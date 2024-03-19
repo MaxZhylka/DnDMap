0b56acff4d3dc4663128a69f89d30a4695f2e636
MaxZhylka <dexhonesta@gmail.com>
2024-03-19 02:05:57 +0200

    Merge after added dependencies between components

---
+++ frontend/src/app/services/character.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CharacterService {
+
+  headValue: string[]  = ['', '', '', '', '', '1'];
+
+  constructor() { }
+}
