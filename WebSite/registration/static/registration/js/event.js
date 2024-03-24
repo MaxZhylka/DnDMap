c2ab58bbc0f7177e8e86035c9ba2739bc69d2458
EKorchevsky <korchevskyeduard@gmail.com>
2024-03-24 20:26:50 +0100

    19.03.2024 треть первой страницы

--- WebSite/registration/static/registration/js/event.js
+++ WebSite/registration/static/registration/js/event.js
@@ -240,6 +240,7 @@ $(function() {
                 $('.strengthmodifier').text(Math.floor((strengthValue - 10) / 2));
             }
         }
+        AthIndex();
     });
     $('[name="dexterity"]').on('input', function() {
         var dexterityValue = parseInt($(this).val());
