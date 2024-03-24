c1750bfe947b99829fa6584097e7c9fe346609a1
EKorchevsky <korchevskyeduard@gmail.com>
2024-03-24 21:51:41 +0100

    19.03.2024 треть первой страницы

--- frontend/src/app/components/character-sheet/passive-perception/passive-perception.component.ts
+++ frontend/src/app/components/character-sheet/passive-perception/passive-perception.component.ts
@@ -10,23 +10,20 @@ export class PassivePerceptionComponent {
   @Input() text: string = '';
   @Input() id: string = '';
   @Input() abilitysave: string = '';
-  inspirOne: string = '../../../assets/img/inspirone.png';
-  inspirTwo: string = '../../../assets/img/inspirtwo.png';
-  currentImage: string = this.inspirOne;
   toggle: boolean = false;
   constructor(private characterService: CharacterService) {
   }
 get calculateSavingThrow(): number {
       let proficiency: number = Math.floor((this.characterService.level - 1) / 4 + 2);
-      let modifire = Math.floor((this.characterService.wisdom - 10) / 2);
+      let modificator = Math.floor((this.characterService.wisdom - 10) / 2);
       let checkBox:boolean = this.characterService.checkboxPerception;
       if (checkBox)
       {
-        return 10 + proficiency + modifire;
+        return 10 + proficiency + modificator;
       }
       else
       {
-        return 10 + modifire;
+        return 10 + modificator;
       }
   }
 }
