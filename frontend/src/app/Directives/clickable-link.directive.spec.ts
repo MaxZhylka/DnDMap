a443475d4a8e0a0bda5fda05a959d776f429525a
MaxZhylka <dexhonesta@gmail.com>
2024-04-08 01:02:21 +0300

    Revert "Выровнял столбики, добавил возможность вставки ссылки"
    This reverts commit 2a5a3600f8b0ac3c10de8d50205c4381314c9b85.

--- frontend/src/app/Directives/clickable-link.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ClickableLinkDirective } from './clickable-link.directive';
-
-describe('ClickableLinkDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ClickableLinkDirective();
-    expect(directive).toBeTruthy();
-  });
-});
