8f38c6fcdb1a3bc4a2ecb706bc4fd47bb832b241
EKorchevsky <korchevskyeduard@gmail.com>
2024-06-03 18:02:36 +0200

    registration and sign in panel disign

--- frontend/src/app/services/character.service.ts
+++ frontend/src/app/services/character.service.ts
@@ -38,7 +38,7 @@ export class CharacterService {
   backstory:string="";
   race:string="";
   worldviews:string="";
-  experience:string="";
+  experience:string="0";
   level:number=1;
   proficiency:number=2;
   //ability value
@@ -278,7 +278,7 @@ export class CharacterService {
     backstory: '',
     race: '',
     worldviews: '',
-    experience: '',
+    experience: '0',
     level: 1,
     proficiency: 2,
     strength: 10,
